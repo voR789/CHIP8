ffe48fb0f9ef3e16ee540645f541b4010eeb8560
voR789 <ryanvo2020@gmail.com>
2025-05-17 15:22:29 -0500

    setup main file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <GL/glut.h>
+#include "chip8.h"
+
+int main(){
+
+    // Setup graphics and input
+
+    // Initialize CPU, and load game
+
+    // Emulation loop
+    /*
+    for(;;){
+    ...
+    }
+    
+    */
+    return 0;
+}
